30c184f5d25f0f9a276be7270bd9835647a28611
nmingosrox <fdomingos116@gmail.com>
2025-07-28 17:34:05 +0200

    Fixed a dependency issuefor creating necessary db files when the app starts; added products live search to the dashboard; considering adding defaukt images for listings without images but imagery is very necessary

--- buxxel_app.py
+++ buxxel_app.py
@@ -3,12 +3,9 @@ from models import db, Product, Vendor
 import os
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
-
-app = Flask(__name__)
 from flask_migrate import Migrate
 
-db.init_app(app)
-migrate = Migrate(app, db)
+app = Flask(__name__)
 
 # Configuration
 app.secret_key = 'your_secret_key_here'
@@ -20,11 +17,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///buxxel.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
@@ -253,6 +247,8 @@ def vendor_settings():
 
     return render_template("vendor_settings.html", vendor=vendor)
 
+with app.app_context():
+    db.create_all()
 
 
 if __name__ == '__main__':
